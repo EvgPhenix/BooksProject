47378f845d3ad15f5f095883b05fe51035e546b5
EvgPhenix <43745474+EvgPhenix@users.noreply.github.com>
2018-12-18 08:31:11 -0500

    Проект книжная полка

---
+++ src/main/java/com/Jonny/logic/Controller/HomeAction.java
@@ -0,0 +1,15 @@
+package com.Jonny.logic.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeAction {
+    @RequestMapping("/")
+    public String homeAction() {
+        return "index";
+    }
+}
